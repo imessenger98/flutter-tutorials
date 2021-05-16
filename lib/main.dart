f4527b7749eedc70a8d0819ba5e93e123df302f3
MUHAMMED YAZEEN <27795050+messenger-1012@users.noreply.github.com>
2021-05-16 21:45:05 +0530

    hello world
    program to print hello world in the center of the app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  MaterialApp app = MaterialApp(
+      title: "muhammed yazeen",
+      home: Scaffold(
+          body: Container(
+            child: Center(
+              child: Text(
+                "HELLO WORLD"
+              ),
+            ),
+            ),
+          ),
+        );
+  runApp(app);
+}
