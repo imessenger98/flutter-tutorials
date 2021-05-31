3a720f425d99f1003592eabe3f5f21bcda47d8ec
MUHAMMED YAZEEN <27795050+messenger-1012@users.noreply.github.com>
2021-05-31 13:24:27 +0530

    widget align(text)

--- lib/widgets/SinglChildLayouts.dart
+++ lib/widgets/SinglChildLayouts.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 class SingleChildLayouts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        width: 200, height: 100, child: Center(child: Text("Muhammed yazeen")));
+    return Align(
+      child: Text("MUhammed yazeen an"),
+      alignment: Alignment.centerLeft,
+    );
   }
 }
