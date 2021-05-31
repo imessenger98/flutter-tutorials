a4a1abd3e1f1a170f6f78976d9b991b0c48d77ed
MUHAMMED YAZEEN <27795050+messenger-1012@users.noreply.github.com>
2021-05-31 12:40:10 +0530

    custom container with height,width,and color

--- lib/widgets/SinglChildLayouts.dart
+++ lib/widgets/SinglChildLayouts.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 class SingleChildLayouts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(100),
-      child: Text("messenger1012"),
+    return Container(
+      width: 400,
+      height: 200,
+      color: Colors.red,
+      child: Center(
+        child: Text("MUHAMMED YAZEEN AN"),
+      ),
     );
   }
 }
